ff59e6c02e364847156125fd991edfe00fd64b87
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2018-04-24 12:34:24 +0900

    [MatrixFactorization] computing u too much time.

--- matrix_factorization/tests/test_matrix_factorization.py
+++ matrix_factorization/tests/test_matrix_factorization.py
@@ -6,73 +6,109 @@ from matrix_factorization.matrix_factorization import NonlinearSemiNMF, SemiNMF
 np.random.seed(42)
 tf.set_random_seed(42)
 
+import time
 
 def test_initializer():
-  print()
-  target_y = tf.random_uniform(shape=(6000, 500), maxval=1)
-  v_shape = (6000, 784)
-  u_shape = (784, 500)
-  print("not use bias")
-  mf = NonlinearSemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, use_bias=False, name='1')
-  mf = SemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, use_bias=False, name='2')
-  print("use bias")
-  mf = NonlinearSemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, name='3')
-  mf = SemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, name='4')
+    print()
+    target_y = tf.random_uniform(shape=(6000, 500), maxval=1)
+    v_shape = (6000, 784)
+    u_shape = (784, 500)
+    print("not use bias")
+    mf = NonlinearSemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, use_bias=False, name='1')
+    mf = SemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, use_bias=False, name='2')
+    print("use bias")
+    mf = NonlinearSemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, name='3')
+    mf = SemiNMF(target_y=target_y, v_shape=v_shape, u_shape=u_shape, name='4')
 
 
 def wrapper_nmf(nmf_class, use_bias=False, minval=0):
-  print("")
-  target = tf.placeholder(tf.float32, [1000, 500])
-  mf = nmf_class(target, (1000, 784), (784, 500), use_bias=use_bias)
-  u_op, v_op, bias_op, loss_op = mf.factorize()
-  init = tf.global_variables_initializer()
-  a = np.random.uniform(minval, 1, size=[1000, 500])
-  with tf.Session() as sess:
-    init.run()
-    for i in range(10):
-      loss, v = sess.run([loss_op, v_op], feed_dict={mf.target_y: a})
-      print("loss", loss, "min(v)", np.min(v), "max(v)", np.max(v))
+    print("")
+    target = tf.placeholder(tf.float32, [1000, 500])
+    mf = nmf_class(target, (1000, 784), (784, 500), use_bias=use_bias)
+    u_op, v_op, bias_op, loss_op = mf.factorize()
+    init = tf.global_variables_initializer()
+    import scipy.io as sio
+    mat = sio.loadmat('test_random.mat')
+    a = mat['mat']
+    with tf.Session() as sess:
+        init.run()
+        
+        global_start_time = time.time()
+        local_duration = 0
+        
+        num_iters = 2
+        for _ in range(num_iters):
+            
+            time.sleep(0.5)
+            
+            start_time = time.time()
+            _ = sess.run([u_op], feed_dict={mf.target_y: a})
+            u_duration = time.time() - start_time
+            local_duration += u_duration
+            print('Duration compute u: {}'.format(u_duration))
+            
+            time.sleep(0.5)
+            
+            start_time = time.time()
+            _ = sess.run([v_op], feed_dict={mf.target_y: a})
+            v_duration = time.time() - start_time
+            local_duration += v_duration
+            print('Duration compute v: {}'.format(v_duration))
+            
+            time.sleep(0.5)
+            
+            start_time = time.time()
+            loss = sess.run([loss_op], feed_dict={mf.target_y: a})
+            loss_duration = time.time() - start_time
+            local_duration += loss_duration
+            print('Duration compute loss: {}, loss: {}'.format(loss_duration, loss))
+        
+        global_end_time = time.time()
+        duration = global_end_time - global_start_time - (0.5 * 3 * num_iters) - local_duration
+        print('Duration compute other process: {}'.format(duration))
+        
 
 
 def nonlinear_semi_nmf():
-  tf.reset_default_graph()
-  target = tf.placeholder(tf.float32, [1000, 500])
-  mf = NonlinearSemiNMF(target, (1000, 784), (784, 500), use_bias=True)
-  u_op, v_op, bias_op, loss_op = mf.factorize()
-
-  a = np.random.uniform(0, 1, size=[1000, 500])
-
-  init = tf.global_variables_initializer()
-  with tf.Session() as sess:
-    init.run()
-    for i in range(10):
-      _, loss = sess.run([u_op, loss_op], feed_dict={mf.target_y: a})
-      print("loss", loss)
-    _, loss = sess.run([v_op, loss_op], feed_dict={mf.target_y: a})
-    print("loss", loss)
+    tf.reset_default_graph()
+    target = tf.placeholder(tf.float32, [1000, 500])
+    mf = NonlinearSemiNMF(target, (1000, 784), (784, 500), use_bias=True)
+    u_op, v_op, bias_op, loss_op = mf.factorize()
+    
+    a = np.random.uniform(0, 1, size=[1000, 500])
+    
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        init.run()
+        for i in range(10):
+            _, loss = sess.run([u_op, loss_op], feed_dict={mf.target_y: a})
+            print("loss", loss)
+        _, loss = sess.run([v_op, loss_op], feed_dict={mf.target_y: a})
+        print("loss", loss)
 
 
 def test_nonlinear_semi_nmf_no_bias():
-  wrapper_nmf(NonlinearSemiNMF, use_bias=False)
+    wrapper_nmf(NonlinearSemiNMF, use_bias=False)
 
 
 def test_nonlinear_semi_nmf_have_bias():
-  wrapper_nmf(NonlinearSemiNMF, use_bias=True)
+    wrapper_nmf(NonlinearSemiNMF, use_bias=True)
 
 
 def test_semi_nmf_no_bias():
-  wrapper_nmf(SemiNMF, use_bias=False)
+    wrapper_nmf(SemiNMF, use_bias=False)
 
 
 def test_semi_nmf_have_bias():
-  wrapper_nmf(SemiNMF, use_bias=True)
+    wrapper_nmf(SemiNMF, use_bias=True)
 
 
 def main(_):
-  # test_semi_nmf_no_bias()
-  # test_nonlinear_semi_nmf_no_bias()
-  nonlinear_semi_nmf()
-
+    # test_semi_nmf_no_bias()
+    # test_nonlinear_semi_nmf_no_bias()
+    test_semi_nmf_no_bias()
+    # test_semi_nmf_have_bias()
+ 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
