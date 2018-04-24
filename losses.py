a2b2485f3ddabcd82d3d18ce0d3a19320902640d
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2018-04-24 12:43:23 +0900

    [Loss] Don't use tensorflow API.

--- losses.py
+++ losses.py
@@ -1,13 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 import tensorflow as tf
 
 
-def frobenius_norm(x, y):
-  losses = tf.subtract(x, y)
-  losses = tf.divide(losses, tf.norm(y))
-  # TODO: HOW TO USE FROBENIOUS
-  loss = tf.square(tf.norm(losses))
-  return loss
+def frobenius_norm(original_x, muled_y):
+    losses = original_x - muled_y
+    losses = tf.norm(losses) / tf.norm(original_x)
+    return losses
 
 
 def np_frobenius_norm(x, y):
